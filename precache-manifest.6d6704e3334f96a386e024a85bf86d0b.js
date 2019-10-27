728f33072adb0c5ec5e024e3235f248971ac89e7
Mateusz Stacel <mateusz.stace92@gmail.com>
2019-10-27 22:17:27 +0100

    Updates

---
+++ precache-manifest.6d6704e3334f96a386e024a85bf86d0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f42767f91ccbd483a6b0262e80eabf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "355a873b06683c4e11da",
+    "url": "/static/css/2.d3811587.chunk.css"
+  },
+  {
+    "revision": "bb5498871fa50c6eb8c5",
+    "url": "/static/css/main.bd232c85.chunk.css"
+  },
+  {
+    "revision": "355a873b06683c4e11da",
+    "url": "/static/js/2.a4b400e4.chunk.js"
+  },
+  {
+    "revision": "bb5498871fa50c6eb8c5",
+    "url": "/static/js/main.7800efbe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
