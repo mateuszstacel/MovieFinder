09993bc52325d861e0e3c0966830e23811c6fa90
Mateusz Stacel <mateusz.stacel92@gmail.com>
2019-10-28 09:58:02 +0000

    ref: Movie details to use interface for response

--- app/src/ui/pages/movieDetails.tsx
+++ app/src/ui/pages/movieDetails.tsx
@@ -1,116 +1,157 @@
-import React, {useState, useEffect} from 'react'
-import {RouteComponentProps} from 'react-router-dom'
-import {NavBar} from '../templates/navbar'
-import {PlayMovieComponent} from '../organism/youtubeModal'
-import {CarouselComponent} from '../organism/movieCarousel'
+import React, { useState, useEffect } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { NavBar } from "../templates/navbar";
+import { PlayMovieComponent } from "../organism/youtubeModal";
+import { CarouselComponent } from "../organism/movieCarousel";
+
+interface IMovieVideo {
+  key: string;
+}
+
+interface IMovieVideoResponse {
+  results: IMovieVideo[];
+}
+
+interface IMovie {
+  poster_path: string;
+  title: string;
+  overview: string;
+  tagline: string;
+  vote_average: number;
+  vote_count: number;
+}
+
+let defaultData: IMovie = {
+  poster_path: "",
+  title: "",
+  overview: "",
+  tagline: "",
+  vote_average: 0,
+  vote_count: 0
+};
 
 type TParams = { id: string };
 
-
-
 enum KindsOfMovies {
-    drama = "with_genres=18&sort_by=vote_average.desc&vote_count.gte=10",
-    best2010 = "/discover/movie?with_genres=12&primary_release_year=2010",
-    withBradPitt = "/discover/movie?with_people=287,819&sort_by=vote_average.desc",
-    lowRated = "/discover/movie?sort_by=popularity.asc"
+  drama = "with_genres=18&sort_by=vote_average.desc&vote_count.gte=10",
+  best2010 = "/discover/movie?with_genres=12&primary_release_year=2010",
+  withBradPitt = "/discover/movie?with_people=287,819&sort_by=vote_average.desc",
+  lowRated = "/discover/movie?sort_by=popularity.asc"
 }
 
+export const MovieDetails: React.FunctionComponent<
+  RouteComponentProps<TParams, any, any>
+> = ({ match }: RouteComponentProps<TParams>) => {
+  const [imgSrc, setImgSrc] = useState("");
+  const [movieKey, setMovieKey] = useState("");
+  const [title, setTitle] = useState("");
+  const [overview, setOverview] = useState("");
+  const [tagline, setTagLine] = useState("");
+  const [voteAverage, setVoteAvarage] = useState("");
+  const [totalVotes, setTotalVotes] = useState("");
+  const [isVideoVisible, setIsVideoVisible] = useState(false);
+  const [showVideo, setShowVideo] = useState(false);
+  const [data, setData] = useState(defaultData);
 
-export const MovieDetails: React.FunctionComponent<RouteComponentProps<TParams, any, any>> = ({ match }: RouteComponentProps<TParams>) => {
-    
-    const [imgSrc, setImgSrc] = useState("");
-    const [movieKey, setMovieKey] = useState("");
-    const [title, setTitle] = useState("");
-    const [overview, setOverview] = useState("");
-    const [tagline, setTagLine] = useState("");
-    const [voteAverage, setVoteAvarage] = useState("");
-    const [totalVotes, setTotalVotes] = useState("");
-    const [isVideoVisible, setIsVideoVisible] = useState(false);
-    const [showVideo, setShowVideo] = useState(false);
- 
-    useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${match.params.id}?api_key=9f471da832491516e75802f839e2bae2`).then(res => {
-            return res.json()
-        }).then(response => {
-            console.log(response)
-            setImgSrc(response.poster_path);
-            setTitle(response.title)
-            setOverview(response.overview);
-            setTagLine(response.tagline);
-            setVoteAvarage(response.vote_average.toString());
-            setTotalVotes(response.vote_count);
-        })
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/${match.params.id}?api_key=9f471da832491516e75802f839e2bae2`
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then((response: IMovie) => {
+        debugger;
+        console.log(response);
+        setData(response);
+        setImgSrc(response.poster_path);
+        setTitle(response.title);
+        setOverview(response.overview);
+        setTagLine(response.tagline);
+        setVoteAvarage(response.vote_average.toString());
+        setTotalVotes(response.vote_count.toString());
+      });
 
-        fetch(`https://api.themoviedb.org/3/movie/${match.params.id}/videos?api_key=9f471da832491516e75802f839e2bae2&language=en-US`)
-        .then(res => {
-            return res.json()
-        }).then(response => {
-            debugger;
-            console.log(response)
-            if(response.results.length > 0)
-            {
-                setMovieKey(response.results[0].key)
-                setIsVideoVisible(true);
-            }else{
-                setShowVideo(false);
-                setIsVideoVisible(false);
-            }
-                
-           
-         
-        })
-    })
+    fetch(
+      `https://api.themoviedb.org/3/movie/${match.params.id}/videos?api_key=9f471da832491516e75802f839e2bae2&language=en-US`
+    )
+      .then(res => {
+        debugger;
+        return res.json();
+      })
+      .then((response: IMovieVideoResponse) => {
+        if (response.results.length > 0) {
+          setMovieKey(response.results[0].key);
+          setIsVideoVisible(true);
+        } else {
+          setShowVideo(false);
+          setIsVideoVisible(false);
+        }
+      });
+  });
 
-        
-useEffect(() => {
-    if(movieKey != ""){
-        setIsVideoVisible(true);
+  useEffect(() => {
+    if (movieKey != "") {
+      setIsVideoVisible(true);
     }
-}, [movieKey])
+  }, [movieKey]);
 
-const HandleVideoClose = () => {
+  const HandleVideoClose = () => {
     setShowVideo(false);
-}
+  };
 
-const HandleVideoClick = () => {
+  const HandleVideoClick = () => {
     setShowVideo(true);
-}
-    return(<div >
-<NavBar/>
+  };
+  return (
+    <div>
+      <NavBar />
 
+      <div className="MovieDetailsMainComponent">
+        {showVideo && (
+          <PlayMovieComponent movieKey={movieKey} onClose={HandleVideoClose} />
+        )}
+        <div className="MovieImage">
+          <img
+            width="300"
+            height="300"
+            src={`https://image.tmdb.org/t/p/w500/${data.poster_path}`}
+          />
+        </div>
 
+        <div className="Description">
+          <label className="title has-text-white">{data.title}</label>
+          <br />
+          <p>
+            <i className="far fa-star has-text-warning"></i>{" "}
+            {data.vote_average.toString()} of {data.vote_count.toString()}{" "}
+            voutes
+          </p>
+          <br />
+          <label className="subtitle has-text-white">{data.tagline}</label>
+          <br />
+          <br /> <br /> <br />
+          {data.overview}
+        </div>
+        {isVideoVisible && (
+          <div className="PlayVideo">
+            {" "}
+            <p className="title has-text-white" onClick={HandleVideoClick}>
+              Watch thriller
+              <br />
+              <i className="fab fa-youtube"></i>
+            </p>{" "}
+          </div>
+        )}
+      </div>
 
-
-<div className="MovieDetailsMainComponent">
-
-       
-                    {showVideo && <PlayMovieComponent movieKey={movieKey} onClose={HandleVideoClose}/>}
-                <div className="MovieImage">
-                <img width="300" height="300" src={`https://image.tmdb.org/t/p/w500/${imgSrc}`}/>
-                </div>
-
-                <div className="Description">
-                    <label className="title has-text-white">{title}</label><br/>
-                    <p><i className="far fa-star has-text-warning"></i> {voteAverage} of {totalVotes} voutes</p><br/> 
-                    <label className="subtitle has-text-white">{tagline}</label><br/>
-                  
-                    <br/> <br/> <br/>
-                    {overview}
-
-
-                   
-                </div>
-                {isVideoVisible && <div className="PlayVideo"> <p className="title has-text-white" onClick={HandleVideoClick}>Watch thriller<br/><i className="fab fa-youtube"></i></p> </div>}
-            </div>
-
-            <CarouselComponent movieType={KindsOfMovies.best2010} title="Explore More" />
-            <CarouselComponent movieType={KindsOfMovies.withBradPitt} />
-            <CarouselComponent movieType={KindsOfMovies.drama} />
-            <CarouselComponent movieType={KindsOfMovies.lowRated} />
-            
-              
-
-        
-            </div>)
-}
-
+      <CarouselComponent
+        movieType={KindsOfMovies.best2010}
+        title="Explore More"
+      />
+      <CarouselComponent movieType={KindsOfMovies.withBradPitt} />
+      <CarouselComponent movieType={KindsOfMovies.drama} />
+      <CarouselComponent movieType={KindsOfMovies.lowRated} />
+    </div>
+  );
+};
