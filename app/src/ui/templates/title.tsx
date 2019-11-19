94664a042efa64f92602396620d7a0fdb3b34b83
Mateusz Stacel <mateusz.stacel92@gmail.com>
2019-11-19 19:40:10 +0000

    feat: responsive tittles

--- app/src/ui/templates/title.tsx
+++ app/src/ui/templates/title.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 
-export const Title: React.FunctionComponent = () => {
+interface IComponent {
+  className?: string;
+}
+
+export const Component: React.FunctionComponent<IComponent> = (
+  props: IComponent
+) => {
   return (
-    <div>
+    <div className={props.className}>
       <div className="Tittle">
-        <label>Your public library to find yours favorite movies.</label>
+        <label className="TittleLabel">
+          Your public library to find yours favorite movies.
+        </label>
         <br />
-        <label className="subtitle">
+        <label className="subtitle subtitleLabel">
           Discover over million videos shared by our generous community.
         </label>
       </div>
@@ -14,3 +23,19 @@ export const Title: React.FunctionComponent = () => {
     </div>
   );
 };
+
+export const Title = styled(Component)`
+  .TittleLabel {
+    @media screen and (min-device-width: 300px) and (max-device-width: 845px) {
+      font-size: 5vw;
+      margin-bottom: 25px;
+    }
+  }
+
+  .subtitleLabel {
+    @media screen and (min-device-width: 300px) and (max-device-width: 845px) {
+      font-size: 3.5vw;
+      color: white;
+    }
+  }
+`;
